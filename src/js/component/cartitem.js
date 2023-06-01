a3ccbcc1be3839d79f2fef82e509ebdd3c8566c2
BenBungle <benstarkart@gmail.com>
2023-06-01 18:06:57 +0000

    codigoupdate

--- src/js/component/cartitem.js
+++ src/js/component/cartitem.js
@@ -1,11 +1,12 @@
 import React, { useContext} from "react";
 import { Context } from "../store/appContext";
+import "../../styles/cartitem.css";
 
 
 export const CartItem = ({pedido}) => {
     console.log("Pedido",pedido)
     return (
-        <div className="text-end">
+        <div className="texto-carrito">
             <p>{pedido.title + " ........... $" + pedido.price}</p>
             
         </div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../img/logo.jpeg";
+import Logo from "../../img/logo.png";
 import ShoppingCarLogo from "../../img/icons8-carrito-de-compras-24.png";
 import "../../styles/navbar.css";
 import { ModalLogin } from "../component/modallogin";
@@ -11,88 +11,216 @@ import swal from "sweetalert";
 
 
 export const Navbar = () => {
-	const { store, actions} = useContext(Context);
+	const { store, actions } = useContext(Context);
 	const navigate = useNavigate();
 
 	const handleClickLogout = () => {
-        actions.logout()
+		actions.logout()
 		swal("¡Bien!", "Has cerrado sesión correctamente :)", "success");
-        navigate("/")
-	
-    }
+		navigate("/")
+
+	}
+	const calculateTotal = (pedidos) => {
+		let total = 0;
+		pedidos.forEach((pedido) => {
+			total += pedido.price;
+		});
+		return total;
+	};
+
+	const handlePayment = () => {
+		const totalAmount = calculateTotal(store.pedidos);
+		console.log("Total amount:", totalAmount);
+	};
 	return (
-		<div className="position-relative">
-		<nav className="navbar navbar-expand-lg bg-body-tertiary">
-			<div className="container-fluid">
-				<Link to="/">
-					<span className="navbar-brand">
-						<img className="img" src={Logo} />
-					</span>
-				</Link>
-				<button id="buttontoggler" className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNavDropdown">
-					<ul id="ul" className="navbar-nav ms-4">
-						<li className="nav-item ms-4">
-							<div id="menu" className="nav-link active" aria-current="page">MENU</div>
-						</li>
-						<li className="nav-item ms-2">
-							<div id="pedidos" className="nav-link" aria-current="page">PEDIDOS</div>
-						</li>
-						<li className="nav-item ms-2">
-							<div id="contacto" className="nav-link" aria-current="page">CONTACTO</div>
-						</li>
-						<li className="nav-item ms-5">
+		<>
+			<nav className="navbar navbar-expand-lg bg-body-tertiary">
+				<div className="container-fluid">
+					<Link to="/">
+						<span className="navbar-brand">
+							<img className="img" src={Logo} />
+						</span>
+					</Link>
+					<button
+						id="buttontoggler"
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavDropdown"
+						aria-controls="navbarNavDropdown"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNavDropdown">
+						<ul id="ul" className="navbar-nav ms-4">
+							<li className="nav-item ms-4">
+								<div id="menu" className="nav-link active" aria-current="page">
+									MENU
+								</div>
+							</li>
+							<li className="nav-item ms-2">
+								<div id="pedidos" className="nav-link" aria-current="page">
+									PEDIDOS
+								</div>
+							</li>
+							<li className="nav-item ms-2">
+								<div id="contacto" className="nav-link" aria-current="page">
+									CONTACTO
+								</div>
+							</li>
+							<li className="nav-item ms-5">
+								{store.token ? (
+									<button type="button" id="logout" onClick={handleClickLogout}>CERRAR SESIÓN</button>
+								) : (
+									<button type="button" id="inicio" onClick={() => actions.setOpenModal(true)}>INICIA SESIÓN</button>
+								)}
+							</li>
+						</ul>
+					</div>
+					<div className="d-flex justify-content-end">
+						<span id="compra">
 							{store.token ? (
-								<button type="button" id="logout" onClick={handleClickLogout}>CERRAR SESIÓN</button>
-							):(
-								<button type="button" id="inicio"  onClick={() =>actions.setOpenModal(true)}>INICIA SESIÓN</button>
+								<Link to="/logeado">
+									<i
+										className="far fa-user fa-2x me-3"
+										style={{ color: "#723209" }}
+									></i>
+								</Link>
+							) : (
+								<i
+									type="button"
+									data-bs-target="#exampleModalToggle"
+									data-bs-toggle="modal"
+									onClick={() => setModalOpen(!modalOpen)}
+									className="far fa-user fa-2x me-3"
+									style={{ color: "#723209" }}
+								></i>
 							)}
-						</li>
-						
-					</ul>
+							<div className="nav-item dropdown">
+								<div className="position-relative">
+									<img
+										className="cart-toggle"
+										data-bs-toggle="dropdown"
+										aria-expanded="false"
+										src={ShoppingCarLogo}
+										style={{ width: "40px" }}
+									/>
+									{store.pedidos.length > 0 && (
+										<div className="notification">
+											<span>{store.pedidos.length}</span>
+										</div>
+									)}
+									<ul className="dropdown-menu dropdown-menu-end mt-4">
+										{store.pedidos.length === 0 ? (
+											<li className="dropdown-item">Vacio</li>
+										) : (
+											<>
+												{store.pedidos.map((pedido, i) => (
+													<li key={i} className="">
+														<CartItem pedido={pedido} />
+													</li>
+												))}
+												<li className="dropdown-item d-flex justify-content-between">
+													<span>Total:</span>
+													<span className="total-amount">${calculateTotal(store.pedidos)}</span>
+												</li>
+												<li className="dropdown-item">
+													<button className="boton-pago" onClick={handlePayment}>Pagar</button>
+												</li>
+
+											</>
+										)}
+									</ul>
+								</div>
+							</div>
+						</span>
+					</div>
 				</div>
-				<div className="d-flex justify-content-end">
-					<span id="compra">
-						{store.token  ?
+			</nav>
+			{store.openModal &&
+				<ModalLogin />
+			}
+		</>
+	);
+};
+
+
+/*
+<div className="position-relative">
+	<nav className="navbar navbar-expand-lg bg-body-tertiary">
+		<div className="container-fluid">
+			<Link to="/">
+				<span className="navbar-brand">
+					<img className="img" src={Logo} />
+				</span>
+			</Link>
+			<button id="buttontoggler" className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul id="ul" className="navbar-nav ms-4">
+					<li className="nav-item ms-4">
+						<div id="menu" className="nav-link active" aria-current="page">MENU</div>
+					</li>
+					<li className="nav-item ms-2">
+						<div id="pedidos" className="nav-link" aria-current="page">PEDIDOS</div>
+					</li>
+					<li className="nav-item ms-2">
+						<div id="contacto" className="nav-link" aria-current="page">CONTACTO</div>
+					</li>
+					<li className="nav-item ms-5">
+						{store.token ? (
+							<button type="button" id="logout" onClick={handleClickLogout}>CERRAR SESIÓN</button>
+						) : (
+							<button type="button" id="inicio" onClick={() => actions.setOpenModal(true)}>INICIA SESIÓN</button>
+						)}
+					</li>
+
+				</ul>
+			</div>
+			<div className="d-flex justify-content-end">
+				<span id="compra">
+					{store.token ?
 						(
 							<Link to="/logeado">
-								<i className="far fa-user fa-2x me-3" style={{color:"#723209"}}></i>
+								<i className="far fa-user fa-2x me-3" style={{ color: "#723209" }}></i>
 							</Link>
 						)
 						:
 						(
-							<i type="button" onClick={() =>actions.setOpenModal(true)} data-bs-target="#exampleModalToggle" data-bs-toggle="modal" className="far fa-user fa-2x me-3" style={{color:"#723209"}}></i>
+							<i type="button" onClick={() => actions.setOpenModal(true)} data-bs-target="#exampleModalToggle" data-bs-toggle="modal" className="far fa-user fa-2x me-3" style={{ color: "#723209" }}></i>
 						)
 					}
-						
-						<div className="nav-item dropdown">
-							<img className="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" src={ShoppingCarLogo} style={{width:"40px"}}/>
-							<ul className="dropdown-menu dropdown-menu-end mt-4">
-								{store.pedidos.length == 0 ? (
-									<li className="dropdown-item">Empty</li> 
-								):(store.pedidos.map((pedido,i) => {
-									return (
-										<li className="dropdown-item d-flex justify-content-between">
-											<CartItem pedido={pedido} />
-										</li>
-										
-									)
-								})
 
-								)}
-							</ul>
-						</div>
-						
-					</span>
-				</div>
-				
+					<div className="nav-item dropdown">
+						<img className="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" src={ShoppingCarLogo} style={{ width: "40px" }} />
+						<ul className="dropdown-menu dropdown-menu-end mt-4">
+							{store.pedidos.length == 0 ? (
+								<li className="dropdown-item">Empty</li>
+							) : (store.pedidos.map((pedido, i) => {
+								return (
+									<li className="dropdown-item d-flex justify-content-between">
+										<CartItem pedido={pedido} />
+									</li>
+
+								)
+							})
+
+							)}
+						</ul>
+					</div>
+
+				</span>
 			</div>
-		</nav>
-			{store.openModal &&
-				<ModalLogin />
-			}
+
 		</div>
-	);
+	</nav>
+	{store.openModal &&
+		<ModalLogin />
+	}
+</div>
+);
 };
+*/
